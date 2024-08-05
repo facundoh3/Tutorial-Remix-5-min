b0b07eac6611f94bfe802644a03ba0f6a71ec50f
Facundo <facundo69@gmail.com>
2024-08-05 12:55:15 -0300

    Añadido: Configure Vite para que sea middleware en el servidor y borre el remix-run/serve que es que lo configura por defecto

--- app/root.jsx
+++ app/root.jsx
@@ -14,6 +14,7 @@ export default function App()
       </head>
       <body>
         <h1>Que onda Locuras Intergalacticas</h1>
+        <h2>Prueba si renderiza en tiempo real y si, lo hace</h2>
         <Outlet />
 
         <Scripts />
